27f3cb0ed832bb899626c81d1a03a690a820107c
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-03-12 15:18:13 +0300

    fix: Linear check not working

--- PostClasses/PostClasses.cs
+++ PostClasses/PostClasses.cs
@@ -110,7 +110,7 @@ public static class PostClasses
                 continue;
 
             bool ifWasLinear = eval[0];
-            for (int j = 1 << (int)Math.Log2(i); j > 0; j >>= 1)
+            for (int j = i - 1; j > 0; j--)
             {
                 if ((j & i) != 0)
                     ifWasLinear ^= eval[j];
